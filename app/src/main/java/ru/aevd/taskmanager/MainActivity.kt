ac5b5c6ef81ef5415c958c7faefc3fc4f0cb2fd8
Aleksandr Evdokimov <alexandr8@gmail.com>
2021-04-26 02:15:59 +0300

    Add view binding, add Calendar

--- app/src/main/java/ru/aevd/taskmanager/MainActivity.kt
+++ app/src/main/java/ru/aevd/taskmanager/MainActivity.kt
@@ -2,10 +2,28 @@ package ru.aevd.taskmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import ru.aevd.taskmanager.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val today = Calendar.getInstance()
+        binding.datePicker.init(
+                today.get(Calendar.YEAR),
+                today.get(Calendar.MONTH),
+                today.get(Calendar.DAY_OF_MONTH)
+        ) {
+            view, year, month, day ->
+            val month = month + 1
+            val msg = "You Selected: $day/$month/$year"
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+        }
     }
 }
